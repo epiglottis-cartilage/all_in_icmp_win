09064b469003fc122d33742e109f7216ba457173
epiglottis <epiglottis.cartilage@gmail.com>
2025-03-21 18:03:04 +0800

    build based on arch

--- build.rs
+++ build.rs
@@ -1,3 +1,9 @@
 fn main() {
+    #[cfg(not(target_family = "windows"))]
+    panic!("Support only windows");
+    
+    #[cfg(target_arch = "x86_64")]
     println!("cargo::rustc-link-search=WinDivert/x64");
+    #[cfg(target_arch = "x86")]
+    println!("cargo::rustc-link-search=WinDivert/x86");
 }
